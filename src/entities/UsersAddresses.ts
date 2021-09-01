8aec182d1bbcdbf4b6f5da89f554090438158ef9
Eduardo Sá Neto <eduardo.albuquerque94@gmail.com>
2021-09-01 15:02:12 -0300

    Create users_addresses middle table

--- src/entities/UsersAddresses.ts
+++ src/entities/UsersAddresses.ts
@@ -2,7 +2,7 @@ import { Entity, PrimaryGeneratedColumn, Column, ManyToOne } from 'typeorm'
 import Users from './Users'
 import Addresses from './Addresses'
 
-@Entity('users_Addresses')
+@Entity('users_addresses')
 export default class UsersAddresses {
     @PrimaryGeneratedColumn()
     id: number;
